1dfa58115e9454e54bb856c040db2d2da81b0449
doondu <rabbit7183@gmail.com>
2024-01-11 17:16:30 +0900

    Add: Add files

--- gym_xplane_final_version/examples/random_agent.py
+++ gym_xplane_final_version/examples/random_agent.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
 	
     
     #parser.add_argument('--client', help='client address',default=client)
-    parser.add_argument('--clientAddr', help='xplane host address', default='0.0.0.0')
+    parser.add_argument('--clientAddr', help='xplane host address', default='127.0.0.1')
     parser.add_argument('--xpHost', help='x plane port', default='127.0.0.1')
     parser.add_argument('--xpPort', help='client port', default=49009)
     parser.add_argument('--clientPort', help='client port', default=1)
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     agent = RandomAgent(env.action_space)
 
     episodes = 0
-    while episodes < 50:
+    while episodes < 1:
         obs = env.reset()
         done = False
         while not done:
